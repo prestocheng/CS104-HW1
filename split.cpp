1032983e59481c7680579551e835c1724023c743
Preston Cheng <chengpre@usc.edu>
2025-09-09 17:06:27 +0000

    Part 1 complete

--- split.cpp
+++ split.cpp
@@ -11,13 +11,36 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
 
 void split(Node*& in, Node*& odds, Node*& evens)
 {
-  /* Add code here */
-// WRITE YOUR CODE HERE
+  // Base case = original list is empty
+  if (in == NULL) {
+    return;
+  }
+
+  // Remove original list and increment in
+  Node* temp = in;
+  in = in->next;
+  temp->next = NULL;
+
+
+  // Value is odd
+  if (temp->value % 2 == 1)  {
+    split(in, odds, evens);
+    temp->next = odds;
+    odds = temp;
+  }
+
+  // Value is even
+  else  {
+    split(in, odds, evens);
+    temp->next = evens;
+    evens = temp;
+  }
 }
 
 /* If you needed a helper function, write it here */
